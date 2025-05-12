94a4a401d347d1f2ad0cb6f4edc4a9f85df4075e
Alex Sunekants (Presidio) <asunekants@MET2LPT501.met.local>
2025-05-12 11:02:26 +0100

    added franovation folder

---
+++ keyboards/handwired/franovation_v1/config.h
@@ -0,0 +1,2 @@
+/* LED indicator pins */
+#define LED_NUM_LOCK_PIN    F5
---
+++ keyboards/handwired/franovation_v1/keymaps/default/keymap.c
@@ -0,0 +1,83 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    KC_P00 = SAFE_RANGE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │Esc│Tab│MO1│Bsp│
+     * ├───┼───┼───┼───┤
+     * │Num│ / │ * │ - │
+     * ├───┼───┼───┼───┤
+     * │ 7 │ 8 │ 9 │ + │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ % │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ = │
+     * ├───┼───┼───┼───┤
+     * │ 0 │00 │ . │Ent│
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_6x4(
+        KC_ESC,  KC_TAB,  MO(1),   KC_BSPC,
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PERC,
+        KC_P1,   KC_P2,   KC_P3,   KC_EQL,
+        KC_P0,   KC_P00,  KC_PDOT, KC_PENT
+    ),
+
+    /*
+     * ┌───┐───┬───┬───┐
+     * │Rst│Tab│MO1│Bsp│
+     * └───┘───┼───┼───┤
+     * │Num│ / │ * │ - │
+     * ┌───┬───┬───┐───┤
+     * │Hom│ ↑ │PgU│ + │
+     * ├───┼───┼───┤───┤
+     * │ ← │   │ → │ % │
+     * ├───┼───┼───┤───┤
+     * │End│ ↓ │PgD│ = │
+     * ├───┼───┼───┤───┤
+     * │Ins│   │Del│Ent│
+     * └───┴───┴───┘───┘
+     */
+    [1] = LAYOUT_ortho_6x4(
+        QK_BOOT,   _______, _______, _______,
+        _______, _______, _______, _______,
+        KC_HOME, KC_UP,   KC_PGUP, _______,
+        KC_LEFT, XXXXXXX, KC_RGHT, _______,
+        KC_END,  KC_DOWN, KC_PGDN, _______,
+        KC_INS,  XXXXXXX, KC_DEL,  _______
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case KC_P00:
+                tap_code(KC_P0);
+                tap_code(KC_P0);
+                return false;
+        }
+    }
+    return true;
+}
+bool led_update_kb(led_t led_state) {
+    bool res = led_update_user(led_state);
+    if(res) {
+        // gpio_write_pin sets the pin high for 1 and low for 0.
+        // In this example the pins are inverted, setting
+        // it low/0 turns it on, and high/1 turns the LED off.
+        // This behavior depends on whether the LED is between the pin
+        // and VCC or the pin and GND.
+        gpio_set_pin_output(LED_NUM_LOCK_PIN);
+        gpio_write_pin(LED_NUM_LOCK_PIN, led_state.num_lock);
+    }
+    return res;
+}
